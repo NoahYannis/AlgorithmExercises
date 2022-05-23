89c8fa9c0a8670fe0f864b36332bc0a5bd288cc7
NoahYannis <noah.guderjahn@gmx.de>
2022-05-23 20:08:18 +0200

    Projektdateien hinzufügen.

---
+++ AlgorithmusÜbungenKonsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFacultyAlgorithm
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of which you want to calculate the faculty: ");
+
+            int result = 0;
+
+            while (result is 0)
+            {
+                try
+                {
+                    string numberString = Console.ReadLine();
+                    int numberInt = Convert.ToInt32(numberString);
+
+                    int facultyLoop = numberInt;
+                    result = numberInt;
+
+                    for (int i = 0; i < facultyLoop; i++)
+                    {
+                        while (facultyLoop != 1)
+                        {
+                            result = result * (facultyLoop - 1);
+                            facultyLoop--;
+                        }
+                    }
+
+                    Console.WriteLine($"The faculty of {numberInt} is: {result}.");
+                    Console.ReadLine();
+                }
+                catch (System.FormatException)
+                {
+                    Console.Write("Please enter a valid number: ");
+                } 
+            }
+        }
+    }
+}
